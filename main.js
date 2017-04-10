5e8dff646b925c7d1f9fa16890910c5b62c525da
方波 <bluepooh@126.com>
2017-04-10 16:36:50 +0800

    exchange proxy app

---
+++ main.js
@@ -0,0 +1,50 @@
+const electron = require('electron');
+const menuTemplate = require('./menu.js');
+const ipcMain = electron.ipcMain;
+const app = electron.app;
+const Menu = electron.Menu;
+const BrowserWindow = electron.BrowserWindow;
+let mainWindow;
+
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        width: 1200,
+        height: 780,
+        icon: __dirname + '/icon.png',
+        // titleBarStyle: 'hidden-inset',
+        backgroundColor: '#fff'
+    });
+
+    mainWindow.setTitle(require('./package.json').name);
+
+    mainWindow.loadURL(`http://59.111.99.122`);
+
+    // mainWindow.loadURL('http://localhost:4000');
+    // mainWindow.webContents.openDevTools();
+
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
+}
+
+function createMenu() {
+    const menu = Menu.buildFromTemplate(menuTemplate);
+    Menu.setApplicationMenu(menu);
+}
+
+app.on('ready', () => {
+    createWindow();
+    createMenu();
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (mainWindow === null) {
+        createWindow();
+    }
+});
